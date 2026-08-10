a280fe4897340e07055185bfadf83b6ee98984d9
curumatipulan-dev <curumatipulan@gmail.com>
2026-08-10 08:38:36 +0300

    fix(build): set Metro project root for release bundling

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
+  const defaultConfig = await getDefaultConfig(__dirname);
   return {
     ...defaultConfig,
+    projectRoot: __dirname,
+    watchFolders: [],
     resolver: {
       ...defaultConfig.resolver,
       assetExts: [...defaultConfig.resolver.assetExts, 'cjs', 'node'],
