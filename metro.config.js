cca9eb287f1b7845fe7edba4ed591f97e8514fd8
curumatipulan-dev <curumatipulan@gmail.com>
2026-08-10 08:48:38 +0300

    fix(android): RN 0.72 gradle config, hermes props, debug keystore in CI

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
+  const defaultConfig = await getDefaultConfig();
   return {
     ...defaultConfig,
-    projectRoot: __dirname,
-    watchFolders: [],
     resolver: {
       ...defaultConfig.resolver,
       assetExts: [...defaultConfig.resolver.assetExts, 'cjs', 'node'],
