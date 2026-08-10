4bac3c1fc5b991ef982a1ef2af71cfc8f7a4be82
curumatipulan-dev <curumatipulan@gmail.com>
2026-08-10 09:17:34 +0300

    fix: use RN metro config

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,9 @@
-const { getDefaultConfig } = require('metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
-  return {
-    ...defaultConfig,
-    resolver: {
-      ...defaultConfig.resolver,
-      assetExts: [...defaultConfig.resolver.assetExts, 'cjs', 'node'],
-    },
-  };
-})();
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
